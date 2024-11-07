dd88aa4dfb7c3cba2b92d0688bd7cb3f21929da1
FSanchez06 <felipesanchezcon2@gmail.com>
2024-11-07 16:15:40 -0500

    Menu interactivo Felipe Sanchez

---
+++ helpers/menu.js
@@ -0,0 +1,65 @@
+const colors = require('colors');
+const inquirer = require('inquirer');
+
+const preguntas = [
+    {
+        type: 'list',
+        name: 'options',
+        message: '¿Qué quieres hacer?'.yellow,
+        choices: [
+            { value: '1', name: `${'1.'.blue} Crear tarea` },
+            { value: '2', name: `${'2.'.blue} Listar tareas` },
+            { value: '3', name: `${'3.'.blue} Listar tareas completas` },
+            { value: '4', name: `${'4.'.blue} Listar tareas pendientes` },
+            { value: '5', name: `${'5.'.blue} Completar tarea(s)` },
+            { value: '6', name: `${'6.'.blue} Borrar tarea` },
+            { value: '0', name: `${'0.'.blue} Salir` }
+        ]
+    }
+];
+
+const menu = async () => {
+    console.clear();
+    console.log('='.repeat(30).green);
+    console.log('       Gestor de Tareas'.white.bold);
+    console.log('='.repeat(30).green);
+
+    const { options } = await inquirer.default.prompt(preguntas);
+    return options;
+};
+
+const pausa = async () => {
+    const question = [
+        {
+            type: 'input',
+            name: 'enter',
+            message: `\nPresione la tecla ${'Enter'.green} para continuar\n`
+        }
+    ];
+    await inquirer.default.prompt(question);
+};
+
+const leerInput = async (message) => {
+    const question = [
+        {
+            type: 'input',
+            name: 'desc',
+            message: message.yellow,
+            validate(value) {
+                if (value.length === 0) {
+                    return 'Por favor ingrese un valor'.red;
+                }
+                return true;
+            }
+        }
+    ];
+
+    const { desc } = await inquirer.default.prompt(question);
+    return desc;
+};
+
+module.exports = {
+    menu,
+    pausa,
+    leerInput
+};
